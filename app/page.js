c1335fae410ec3a0e5c3c7f2aed59ddfefeac811
wasif kareem <wasifkareem2762@gmail.com>
2023-09-15 17:31:51 +0530

    project added

--- app/page.js
+++ app/page.js
@@ -10,32 +10,34 @@ import Image from "next/image";
 import photodp from "../public/photodp.jpg";
 import projectpic from "../public/projectpic.png";
 import portfolio from "../public/portfolio.png";
+import course from "../public/course.png";
 
 export default function Home() {
   return (
     <main className=" bg-white text-black ">
-      <section className=" min-h-screen px-6 bg-gradient-to-b from-white to-gray-200  sm:px-44">
-        <nav className=" py-10 mb-8 flex justify-between">
-          <h1 className=" text-xl font-burtons items-center flex font-bold">
+      <section className=" min-h-screen px-6 bg-gradient-to-b from-white to-gray-200  sm:px-36">
+        <nav className=" py-10 mb-8 flex justify-between ">
+          <h1 className=" text-lg font-burtons items-center flex font-bold">
             Wasif Kareem
           </h1>
-          <ul className=" flex items-center">
-            <li className=" sm:mr-4">
+          <ul className=" flex items-center ">
+            <li className=" sm:mr-4 animate-pulse hover:animate-none hover:scale-105 duration-200">
               <a
                 target="_blank"
                 href="https://drive.google.com/file/d/1e6az0eo6rdrbFIg53SRHK1vp9llTkLTj/view?usp=sharing"
               >
                 Resume
               </a>{" "}
-              <hr className="  mt-1 bg-teal-500 sm:h-1 " />
+              <hr className="  mt-1 bg-teal-500 sm:h-1 h-1  " />
             </li>
-            <li className=" bg-gradient-to-r from-cyan-500 to-teal-500 py-2 rounded-md sm:px-4 px-2 text-white ml-2">
+
+            <li className="  transition-all scale-95 hover:scale-100  hover:bg-gradient-to-r hover:from-purple-500 hover:to-yellow-500 bg-gradient-to-r from-cyan-500 to-teal-500 py-2 rounded-md sm:px-4 px-2 text-white ml-2">
               <a href="#project">Projects</a>
             </li>
           </ul>
         </nav>
         <div className=" sm:flex sm:flex-row justify-between sm:items-center">
-          <div className=" pt-20">
+          <div className=" sm:pt-20">
             <h2 className=" text-3xl py-2 font-bold sm:text-5xl text-gray-800">
               About Me
             </h2>
@@ -47,19 +49,19 @@ export default function Home() {
               <a
                 className=" text-teal-500"
                 target="_blank"
-                href="https://twitter.com/Wasif83794508"
+                href="https://www.linkedin.com/in/wasifdev2762/"
               >
-                Twitter.
+                LinkedIn.
               </a>
             </p>
             <hr className=" h-1.5 bg-teal-600  mb-16 w-28" />
           </div>
 
-          <div className=" relative mt-10 flex justify-center">
+          <div className="  relative mt-10 flex justify-center">
             <Image
               alt="wasif"
               src={photodp}
-              className="  sm:max-w-sm sm:h-80 rounded-lg h-48 min-w-max  object-cover shadow-xl "
+              className="  sm:duration-300  sm:max-w-xs sm:h-80 rounded-lg h-48 min-w-max  object-cover shadow-xl "
             />
           </div>
         </div>
@@ -71,6 +73,53 @@ export default function Home() {
             <h2 id="project" className=" text-3xl py-2 font-bold mb-20">
               Projects
             </h2>
+          </div>
+
+          <div className="max-w-md  mt-20 mb-20 mx-auto bg-white rounded-xl shadow-xl overflow-hidden md:max-w-5xl md:min-h-[22rem]">
+            <div className="md:flex">
+              <div className="md:shrink-0">
+                {" "}
+                <Image
+                  className="h-48 w-full object-cover md:min-h-[22rem]  md:w-auto"
+                  src={course}
+                  alt="Modern building architecture"
+                />
+              </div>
+              <div className=" md:flex md:flex-col  md:justify-between ">
+                <div className=" p-8">
+                  <div className="uppercase tracking-wide text-md font-bold">
+                    Course selling website
+                  </div>
+
+                  <p className="mt-2 text-slate-500">
+                    Fully functional Course selling website powered by MERN
+                    stack, Redux & Tailwind.
+                  </p>
+                </div>
+                <div className="flex gap-4  justify-start px-4 font-semibold py-2">
+                  <a target="_blank" href="https://fabina-school.vercel.app/">
+                    <span className=" bg-red-500 rounded p-1">Live demo</span>
+                  </a>
+
+                  <a
+                    target="_blank"
+                    href="https://github.com/wasifkareem/Fabina_school"
+                  >
+                    <span className="rounded p-1 bg-slate-900 text-white">
+                      Project Repo
+                    </span>
+                  </a>
+                </div>
+                <div className=" text-3xl flex justify-between gap-2 px-8 py-4 bg-gray-400">
+                  <BiLogoReact color="blue" />
+                  <BiLogoMongodb color="green" />
+                  <SiExpress color="" />
+                  <FaNode />
+                  <SiRedux color="purple" />
+                  <SiTailwindcss color="cyan" />
+                </div>
+              </div>
+            </div>
           </div>
 
           <div className="max-w-md  mx-auto bg-white rounded-xl shadow-xl overflow-hidden md:max-w-5xl md:min-h-[22rem]">
@@ -217,10 +266,7 @@ export default function Home() {
                 <div className=" text-3xl flex  gap-12 px-8 py-4 bg-gray-400">
                   <BiLogoReact color="blue" />
                   <SiNextdotjs color="black" />
-                  <SiTailwindcss
-                    color="rgb(56, 189, 248)
-"
-                  />
+                  <SiTailwindcss color="cyan" />
                 </div>
               </div>
             </div>
